6e7abec0a35f7df35f49fbff17f3fdcf4360a78a
Santiago <grecodominguezsantiago@gmail.com>
2026-06-02 14:09:48 +0200

    Generación de sitemap para español e inglés

--- apps/web/next-sitemap.config.js
+++ apps/web/next-sitemap.config.js
@@ -3,4 +3,20 @@ module.exports = {
   siteUrl: "https://umbrachat.org",
   generateRobotsTxt: true, // También genera el robots.txt
   sitemapSize: 7000,
+  sourceDir: "./.next",
+  outDir: "./public",
+  additionalPaths: async (config) => {
+    const result = [];
+    const locales = ["es", "en"]; // Tus idiomas configurados
+    const pages = ["", "/about", "/contact", "/info", "/search", "/config"]; // dejé la home vacía para que no duplique la barra
+
+    for (const locale of locales) {
+      for (const page of pages) {
+        // Esto generará: /es, /es/contact, /en, /en/contact...
+        result.push(await config.transform(config, `/${locale}${page}`));
+      }
+    }
+
+    return result;
+  },
 };
