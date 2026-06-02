2e8a44beeb702303015e92ccb48caf164b39268e
Santiago <grecodominguezsantiago@gmail.com>
2026-06-02 13:51:58 +0200

    Generación de sitemap

---
+++ apps/web/next-sitemap.config.ts
@@ -0,0 +1,6 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+  siteUrl: "https://umbrachat.org",
+  generateRobotsTxt: true, // También genera el robots.txt
+  sitemapSize: 7000,
+};
