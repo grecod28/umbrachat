3abcd8982953cd1eb19907f198bf7e8e0463d10f
Santiago <grecodominguezsantiago@gmail.com>
2026-06-15 18:31:42 +0200

    Header corregido en pantallas grandes

--- apps/web/components/layout/headers.tsx
+++ apps/web/components/layout/headers.tsx
@@ -15,18 +15,20 @@ export async function RootHeader() {
   ];
 
   return (
-    <header className="fixed top-0 left-0 w-full flex items-center justify-between px-4 py-2 md:px-16 md:py-6 bg-surface/25 backdrop-blur-md border-b border-primary/10 z-50">
-      <Navbar routes={routes} />
+    <header className="fixed top-0 left-0 w-full  bg-surface/25 backdrop-blur-md border-b border-primary/10 z-50">
+      <div className="max-w-320 w-full  flex items-center justify-between px-4 py-2 md:px-16 md:py-6 mx-auto">
+        <Navbar routes={routes} />
 
-      <section className="flex gap-3 items-center">
-        <Link href="/search" className="hover:text-primary transition-colors">
-          <IoSearch size={24} />
-        </Link>
+        <section className="flex gap-3 items-center">
+          <Link href="/search" className="hover:text-primary transition-colors">
+            <IoSearch size={24} />
+          </Link>
 
-        <Link href="/config" className="hover:text-primary transition-colors">
-          <IoSettingsOutline size={24} />
-        </Link>
-      </section>
+          <Link href="/config" className="hover:text-primary transition-colors">
+            <IoSettingsOutline size={24} />
+          </Link>
+        </section>
+      </div>
     </header>
   );
 }
